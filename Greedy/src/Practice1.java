352caf172d67b5242241931cb69ae93d7ddca992
ashjang <vkdvkdgkfla@naver.com>
2023-06-07 17:06:15 +0900

    greedy 알고리즘 - 수강신청, 거스름돈

---
+++ Greedy/src/Practice1.java
@@ -0,0 +1,46 @@
+/*
+    Activity Selection Problem: 수강신청
+ */
+
+import java.util.ArrayList;
+import java.util.Collections;
+
+class Activity {
+    String name;
+    int start;
+    int end;
+
+    public Activity(String name, int start, int end) {
+        this.name = name;
+        this.start = start;
+        this.end = end;
+    }
+}
+
+public class Practice1 {
+    public static void selectActivity(ArrayList<Activity> list) {
+        // 종료 시간 기준으로 오름차순
+        Collections.sort(list, (x1, x2) -> x1.end - x2. end);
+
+        int curTime = 0;
+        ArrayList<Activity> result = new ArrayList<>();
+        for (Activity a : list) {
+            if (curTime <= a.start) {
+                curTime = a.end;
+                result.add(a);
+            }
+        }
+
+        result.stream().forEach(x -> System.out.print(x.name + "\t"));
+    }
+
+    public static void main(String[] args) {
+        ArrayList<Activity> list = new ArrayList<>();
+        list.add(new Activity("A", 1, 5));
+        list.add(new Activity("B", 4, 5));
+        list.add(new Activity("C", 2, 3));
+        list.add(new Activity("D", 4, 7));
+        list.add(new Activity("E", 6, 10));
+        selectActivity(list);
+    }
+}
---
+++ Greedy/src/Practice2.java
@@ -0,0 +1,42 @@
+/*
+    거스름돈 문제
+ */
+
+import java.util.HashMap;
+import java.util.Map;
+
+public class Practice2 {
+    public static void getChangeCoins(int receivedMoney, int price) {
+        final int[] coins = {500, 100, 50, 10, 5, 1};
+
+        // 어떤 동전이 몇 개인지
+        HashMap<Integer, Integer> result = new HashMap<>();
+
+        // 잔돈 계산
+        int change = receivedMoney - price;
+        // 잔돈 개수
+        int cnt = 0;
+
+        for (int i = 0; i < coins.length; i++) {
+            if (change < coins[i]) {
+                continue;
+            }
+
+            int q = change / coins[i];
+            result.put(coins[i], result.getOrDefault(coins[i], 0) + q);
+
+            change %= coins[i];
+            cnt += q;
+        }
+
+        System.out.println("거스름돈 동전 개수: " + cnt);
+        for (Map.Entry<Integer, Integer> item : result.entrySet()) {
+            System.out.println(item.getKey() + ": " + item.getValue());
+        }
+    }
+
+    public static void main(String[] args) {
+        getChangeCoins(1000, 100);
+        getChangeCoins(1234, 500);
+    }
+}
